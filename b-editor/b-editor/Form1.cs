2a30e122174481cb21a49778026c7b5585d5e704
doldamul <junha0504@gmail.com>
2024-05-13 00:12:49 +0900

    앱 시작시 포스트 열기/앱 종료시 포스트 저장

--- b-editor/b-editor/Form1.cs
+++ b-editor/b-editor/Form1.cs
@@ -3,6 +3,7 @@
     public partial class Form1 : Form
     {
         Settings settings; 
+        string[] postings;
 
         public Form1()
         {
@@ -17,11 +18,13 @@
         private void Form1_Load(object sender, EventArgs e)
         {
             settings = Settings.Load();
+            loadPostings();
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             settings.Save();
+            savePost(settings.currentPost);
 
             if (ExitQuestion() == false) e.Cancel = true;
         }
@@ -36,6 +39,29 @@
                 );
 
             return (result == DialogResult.Yes);
+        }
+
+        private void loadPostings()
+        {
+            postings = Directory.GetFiles(settings.savePath, "*.rtf");
+
+            // 사이드바에 rtf 파일 목록 표시
+
+            // 첫번째 포스트 선택
+            settings.currentPost = postings[0];
+            openPost(settings.currentPost);
+        }
+
+        private void savePost(string filename)
+        {
+            // 현재 열려있는 포스트 저장
+            textEditor.SaveFile(filename);
+        }
+
+        private void openPost(string filename)
+        {
+            // 해당 포스트 선택해서 편집기에 열기
+            textEditor.LoadFile(filename);
         }
     }
 }
